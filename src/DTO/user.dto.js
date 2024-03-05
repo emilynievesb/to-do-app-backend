ab2abcb6bcc66eb7335b3b5a698a03f884b8b23c
Emily <nemily1710@gmail.com>
2024-03-05 11:58:49 -0500

    feat: :sparkles: DTO para la creación de un usuario
    Se realiza un DTO de usuario para validar la llegada de caracteres obligatorios en el formato correcto

---
+++ src/DTO/user.dto.js
@@ -0,0 +1,20 @@
+import { object, string, number, date } from 'yup';
+
+const createUserDTO = async (req, res, next) => {
+    try {
+        const userSchema = object({
+            name: string().required('El nombre es requerido'),
+            username: string().required('El nombre de usuario es requerido'),
+            password: string()
+                .required('La contraseña es requerida')
+                .matches(/^(?=.*[A-Z])(?=.*[0-9]).{8,}$/, 'La contraseña debe tener al menos una mayúscula, un número y ser de al menos 8 caracteres'),
+        });
+        req.DTO = await userSchema.validate(req.body);
+        console.log(req.DTO);
+        next();
+    } catch (error) {
+        res.status(400).json({ status: 'fail', message: error.errors });
+    }
+};
+
+export default createUserDTO;
