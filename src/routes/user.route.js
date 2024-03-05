9b1b0ae2e595792044c934e25d1bcbc3e29de98c
Emily <nemily1710@gmail.com>
2024-03-05 15:47:05 -0500

    feat: :sparkles: Se realiza ruta de login para el usuario
    Esta ruta usa un DTO, y el controlador respectivo para hacer el logeo

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,14 @@
 import Router from 'express';
 import userController from '../controller/user.ctrl.js';
-import createUserDTO from '../DTO/user.dto.js';
+import { createUserDTO, loginUserDTO } from '../DTO/user.dto.js';
 
 const userInitRoute = () => {
     const router = Router();
 
     //? Ruta para crear un nuevo usuario
     router.post('/create-user/', createUserDTO, userController.createNewUser);
+    //? Ruta para logear un usuario
+    router.post('/login-user/', loginUserDTO, userController.loginUser);
 
     return router;
 };
