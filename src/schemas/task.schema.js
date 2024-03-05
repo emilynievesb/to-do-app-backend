c662e54001c65e56e54f43b29637df165657a101
Emily <nemily1710@gmail.com>
2024-03-05 16:37:21 -0500

    refactor: :recycle: Se refactoriza manera en que se tenían los schemas de usuario y tarea

---
+++ src/schemas/task.schema.js
@@ -0,0 +1,13 @@
+import { Schema } from 'mongoose';
+
+const taskSchema = new Schema({
+    id: { type: Number, required: true, unique: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    creation_date: { type: Date, default: Date.now },
+    dead_date: { type: Date, required: true },
+    degree: { type: String, enum: ['Muy importante', 'Importante', 'Normal'], default: 'Normal' },
+    status: { type: Boolean, default: false },
+});
+
+export default taskSchema;
--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,13 +1,5 @@
 import { Schema, model } from 'mongoose';
-
-const taskSchema = new Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    creation_date: { type: Date, default: Date.now },
-    dead_date: { type: Date, required: true },
-    degree: { type: String, enum: ['Muy importante', 'Importante', 'Normal'], default: 'Normal' },
-    status: { type: Boolean, default: false },
-});
+import taskSchema from './task.schema.js';
 
 // Define schema for user
 const userSchema = new Schema({
