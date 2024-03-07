c652b5f6d096e5534bd2c7f859272a4cfe22acef
Emily <nemily1710@gmail.com>
2024-03-06 21:29:03 -0500

    fix: :bug: Se soluciona bug de llaves duplicadas, que estaban haciendo que no se permitiera la creación normal de usuario
    Se soluciona agregando un id único de tipo string

--- src/schemas/task.schema.js
+++ src/schemas/task.schema.js
@@ -1,7 +1,7 @@
 import { Schema } from 'mongoose';
 
 const taskSchema = new Schema({
-    id: { type: Number, required: true, unique: true },
+    id: { type: String, required: true, unique: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
     creation_date: { type: Date, default: Date.now },
